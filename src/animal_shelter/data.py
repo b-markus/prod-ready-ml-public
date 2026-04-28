af073bd2e33f4395130779cea4b18f31b5c105b5
b_markus <10040793+b-markus@users.noreply.github.com>
2026-04-28 14:52:21 +0200

    [solution] Solving exercise of 03_quality_checks.ipynb by adding type annotations

--- src/animal_shelter/data.py
+++ src/animal_shelter/data.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 
 
-def load_data(path):
+def load_data(path: str) -> pd.DataFrame:
     """Load the data and convert the column names.
 
     Parameters
@@ -26,7 +26,7 @@ def load_data(path):
     return df
 
 
-def convert_camel_case(name):
+def convert_camel_case(name: str) -> str:
     """Convert camelCaseString to snake_case_string."""
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()

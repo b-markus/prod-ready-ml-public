9e932e3e1f445955777748e0db99024ba3286a72
b_markus <10040793+b-markus@users.noreply.github.com>
2026-04-28 12:24:48 +0200

    committing changes to data.py

--- src/animal_shelter/data.py
+++ src/animal_shelter/data.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import re
+
+import pandas as pd
+
 
 def load_data(path):
     """Load the data and convert the column names.
@@ -8,10 +10,12 @@ def load_data(path):
     ----------
     path : str
         Path to data
+
     Returns
     -------
     df : pandas.DataFrame
         DataFrame with data
+
     """
     df = (
         pd.read_csv(path, parse_dates=["DateTime"])
